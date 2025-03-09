438f65f8e14172461954d505662f44ee17566f83
espurr <espurr@espurr.com>
2025-03-09 19:11:51 +0900

    Initial Commit

---
+++ Wyrdeer/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Wyrdeer
+//
+//  Created by espurr on 3/9/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
